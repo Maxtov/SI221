98e7e43eb5983acf6769eabe81a662b698250898
MaxTov <maxime.thauvin@telecom-paris.fr>
2020-03-04 19:26:05 +0100

    tp1 q2_1 matrice label ok

--- TP1/q1.py
+++ TP1/q1.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import random, math
+import tiilab
 from sklearn.datasets import make_blobs
 
 def generate_data(sigma):
 	X,y = make_blobs(n_samples=200, centers=[[-1,0],[1,0]], n_features=2, cluster_std=sigma)
-	return X,y
+	return add_bias(X),y
 
 def add_bias(X):
 	newX = []
@@ -23,7 +24,6 @@ def predict(x, w):
 
 def train(data, y,eta, T,w):
 	for i in range(T):
-		sum_error = 0.0
 		for i in range(len(data)):
 			prediction = predict(data[i], w)
 			if(prediction != y[i]):
@@ -40,8 +40,8 @@ def do_N_perceptron(N,eta,T,sigmas):
 	for sigma in range(len(sigmas)):
 		w = np.zeros(3)
 		for n in range(N):
-			X_no_bias,y = generate_data(sigmas[sigma])
-			X = add_bias(X_no_bias)
+			X,y = generate_data(sigmas[sigma])
+			#X = add_bias(X_no_bias)
 			w = train(X,y,eta,T,w)
 			erreurs[sigma][n] = calcul_erreur(X,y,w)
 		e[sigma] = np.array(erreurs[sigma]).mean()
@@ -53,7 +53,7 @@ def calcul_erreur(data,y,w):
 	for i in range(len(data)):
 		if(predict(data[i],w)!=y[i]):
 			err += 1
-	return err/len(data)
+	return err
 
 def calcul_deviation(erreurs,e,N):
 	sum=0
@@ -62,27 +62,126 @@ def calcul_deviation(erreurs,e,N):
 	s = np.sqrt(sum/50)
 	return s
 
+def question1_1():
+	T = 200
+	sigmas = [0.05,0.25,0.5,0.75]
+	w, err, s = do_N_perceptron(50,0.1,T,sigmas)
+	print(w)
+	print("taux d'erreurs et deviation : ",err)
+	print("deviation : ",s)
 
-#plt.show(a)
-T = 200
-sigmas = [0.05,0.25,0.5,0.75]
-w, err, s = do_N_perceptron(50,0.1,T,sigmas)
-print(w)
-print("taux d'erreurs et deviation : ",err)
-print("deviation : ",s)
+	r = range(len(err))
+	plt.title("Erreur et Deviation en fonction de sigma")
+	plt.bar(r, err, width = 0.4, color = 'red',
+	           edgecolor = 'black', linewidth = 1, label='erreur')
+	plt.bar([x + 0.4 for x in r], s, width = 0.4, color = 'blue',
+	           edgecolor = 'black', linewidth = 1, label='deviation')
+	plt.legend()
+	plt.xticks([r1 + 0.4 / 2 for r1 in r], ['0.05', '0.25', '0.5', '0.75'])
+	plt.show()
 
-r = range(len(err))
-plt.title("Erreur et Deviation en fonction de sigma")
-plt.bar(r, err, width = 0.4, color = 'yellow',
-           edgecolor = 'black', linewidth = 1, label='erreur')
-plt.bar([x + 0.4 for x in r], s, width = 0.4, color = 'pink',
-           edgecolor = 'black', linewidth = 1, label='deviation')
-plt.legend()
-plt.xticks([r1 + 0.4 / 2 for r1 in r], ['0.05', '0.25', '0.5', '0.75'])
+def do_N_perceptron_with_p(N,eta,T,p):
+	erreurs = np.zeros((4,N))
+	e=np.zeros(4)
+	s = np.zeros(4)
+	for k in range(len(p)):
+		w = np.zeros(3)
+		for n in range(N):
+			X,y = generate_data(0.15)
+			for i in range(len(X)):
+				rd = random.random()
+				if(rd<p[k]):
+					y[i] = 1 - y[i]
+			w = train(X,y,eta,T,w)
+			erreurs[k][n] = calcul_erreur(X,y,w)
+		e[k] = np.array(erreurs[k]).mean()
+		s[k] = calcul_deviation(erreurs[k],e[k],N)
+	return w, e,s
+
+def question1_2():
+	T = 200
+	N=50
+	p = [0,0.05,0.10,0.20]
+	w, err, s = do_N_perceptron_with_p(N,0.1,T,p)
+		
+	print(w)
+	print("taux d'erreurs et deviation : ",err)
+	print("deviation : ",s)
+
+
+	r = range(len(err))
+	plt.title("Erreur et Deviation en fonction de sigma")
+	plt.bar(r, err, width = 0.4, color = 'red',
+	           edgecolor = 'black', linewidth = 1, label='erreur')
+	plt.bar([x + 0.4 for x in r], s, width = 0.4, color = 'blue',
+	           edgecolor = 'black', linewidth = 1, label='deviation')
+	plt.legend()
+	plt.xticks([r1 + 0.4 / 2 for r1 in r], ['0%', '5%', '10%', '20%'])
+	plt.show()
+
+def evaluate_pixels(image,value):
+	nb_rows = len(image[0])
+	nb_cols = len(image[0][0])
+	labels=np.zeros((nb_rows,nb_cols))
+	for row in range(nb_rows):
+		for col in range(nb_cols):
+			if(image[0][row][col] < value):
+				labels[row][col] = 0
+			else:
+				labels[row][col] = 1
+	return labels
+
+def add_bias_image(image):
+	newImg = []
+	for row in image:
+		newCol = []
+		for pixel in row:
+			newCol.append((pixel,1))
+		newImg.append(newCol)
+	img=np.array(newImg)
+	return img
+
+def error_correction(image, labels, eta, weights):
+	erreurs = []
+	for row in range(len(image)):
+		weights = train(image[row],labels[row],eta,1,weights)
+		erreurs.append(calcul_erreur(image[row],labels[row],weights))
+	erreur = np.sum(erreurs)
+	return weights,erreur
+
+
+
+#img[0] contient les pixels
+img=tiilab.imz2mat("data/landsattarasconC4.ima")
+lab = evaluate_pixels(img,30)
+
+dataset = add_bias_image(img[0])
+
+print(dataset[:][0][:,0])
+
+plt.scatter(lab[0],dataset[:][0][:,0])
 plt.show()
 
+err=-1
+eta = 1
+w = np.zeros(2)
+
+epochs = 0
+while err != 0:
+	w,err = error_correction(dataset,lab,eta,w)
+	epochs += 1
+	print(err)
+
+print(w)
+print(epochs)
 
 '''
+plt.imshow(lab)
+plt.show(block = False)
+tiilab.visusar(img[0])
+'''
+
+''' Afficher un set de donnée et la droite à partir de w
 X,y = make_blobs(n_samples=200, centers=[[-1,0],[1,0]], n_features=2, cluster_std=sigma)
 plt.scatter(X[:,0],X[:,1],c=y)
 abs = np.arange(-1,1,0.01)
